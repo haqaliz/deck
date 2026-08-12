3bbba04148e177d536f60e9467eabbf95efb493b
Ali Haqiqi <aliz@foresightanalytics.ca>
2026-08-12 20:19:44 +0330

    docs: register per-core CPU lines in README and ROADMAP, remove scratch test package

--- Sources/LiveBoxCore/CpuTicks.swift
+++
@@ -1,8 +0,0 @@
-struct CpuTicks: Equatable {
-    var user: UInt64 = 0
-    var system: UInt64 = 0
-    var idle: UInt64 = 0
-    var nice: UInt64 = 0
-
-    var total: UInt64 { user + system + idle + nice }
-}
